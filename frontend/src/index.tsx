4783b2fd209c694d2b345f7f2d3f8d303677081b
Willy McNamara <wmcnamara11@gmail.com>
2024-02-03 11:56:33 -0700

    Added react router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
+import router from './routes'
+import {
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 
-const App = () => {
-  return <div>Hello, Worldio!</div>;
-};
+const rootElement = document.getElementById("root");
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+if (rootElement) {
+  createRoot(rootElement).render(
+    <RouterProvider router={router} />
+  );
+} else {
+  console.error("Root element with id 'root' not found.");
+}
 
 
