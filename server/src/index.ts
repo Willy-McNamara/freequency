ef3c0cee767296645944b74fbbe4efc07b873e2f
Willy McNamara <wmcnamara11@gmail.com>
2024-02-03 10:34:28 -0700

    restructuring repo for separate backend/frontend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,9 @@
-import express, { Request, Response } from 'express';
+import express, {Request, Response} from 'express';
 
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
+  res.send('Hello, World! from backend');
 });
 
 const port = process.env.PORT || 3000;
