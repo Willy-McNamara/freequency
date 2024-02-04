d39d0c3a570379920c51fdb22adea08b2d4407fe
Willy McNamara <wmcnamara11@gmail.com>
2024-02-04 09:43:05 -0700

    Update menu

--- frontend/src/Root.tsx
+++ frontend/src/Root.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
-import { Flex, Box } from '@chakra-ui/react';
+import { Flex, Box, Heading, Icon } from '@chakra-ui/react';
+import { TbHandRock } from 'react-icons/tb';
+import { CgProfile } from 'react-icons/cg';
+import { PiMusicNotesFill } from 'react-icons/pi';
 
 const Root = () => {
   return (
@@ -8,14 +11,29 @@ const Root = () => {
       <h1>Route shell</h1>
       <Flex
         position="absolute"
-        left="10vw"
-        top="10vh"
+        left="2vw"
+        top="30vh"
         direction="column"
-        align="center"
+        align="left"
       >
-        <Link to="Feed">Feed</Link>
-        <Link to="Profile">Profile</Link>
-        <Link to="Practice">Practice</Link>
+        <Link to="Feed">
+          <Flex direction="row" align="center" m="0.25rem">
+            <Icon as={TbHandRock} boxSize="3em" mr="0.5rem"></Icon>
+            <Heading>Feed</Heading>
+          </Flex>
+        </Link>
+        <Link to="Profile">
+          <Flex direction="row" align="center" m="0.25rem">
+            <Icon as={CgProfile} boxSize="3em" mr="0.5rem"></Icon>
+            <Heading>Profile</Heading>
+          </Flex>
+        </Link>
+        <Link to="Practice">
+          <Flex direction="row" align="center" m="0.25rem">
+            <Icon as={PiMusicNotesFill} boxSize="3em" mr="0.5rem"></Icon>
+            <Heading>Practice</Heading>
+          </Flex>
+        </Link>
       </Flex>
       <Box m="3.5rem"></Box>
       <Outlet />
