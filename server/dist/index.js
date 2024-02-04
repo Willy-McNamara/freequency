bd36dfd60f6688911dada7c86a7a4c4d0073338f
Willy McNamara <wmcnamara11@gmail.com>
2024-02-04 10:42:44 -0700

    Serve up frontend build through backend express app

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,10 +4,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const morgan_1 = __importDefault(require("morgan"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
-app.get('/', (req, res) => {
-    res.send('Hello, World! from backend');
-});
+// middleware
+app.use((0, cors_1.default)());
+// custom logging func, can play with later..
+app.use((0, morgan_1.default)(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, "content-length"),
+        "-",
+        tokens["response-time"](req, res),
+        "ms",
+    ].join(" ");
+}));
+app.use(express_1.default.static(path_1.default.join(__dirname, "../../frontend/dist")));
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("Hello, World! from backend");
+// });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,28 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import path from "path";
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World! from backend');
-});
+// middleware
+app.use(cors());
+// custom logging func, can play with later..
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+    ].join(" ");
+  })
+);
+// serve up front end (this mostly works, but some of the components act slightly different.. look into it later)
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
