cc870577afb36511c1078622b8b549c70d50f128
Willy McNamara <wmcnamara11@gmail.com>
2024-02-04 18:42:12 -0700

    Basic Postgres database connection set up, database tables created locally!

--- server/src/entity/Session.ts
+++ server/src/entity/Session.ts
@@ -1,7 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 @Entity()
-export class Member {
+export class Session {
   @PrimaryGeneratedColumn()
   sessionId: string;
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import morgan from "morgan";
 import path from "path";
+require("dotenv").config();
 
 const app = express();
 
@@ -28,24 +29,24 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 const port = process.env.PORT || 3000;
 
-// AppDataSource.initialize()
-//   .then(async () => {
-//     // console.log("Inserting a new user into the database...")
-//     // const user = new User()
-//     // user.firstName = "Timber"
-//     // user.lastName = "Saw"
-//     // user.age = 25
-//     // await AppDataSource.manager.save(user)
-//     // console.log("Saved a new user with id: " + user.id)
+AppDataSource.initialize()
+  .then(async () => {
+    // console.log("Inserting a new user into the database...")
+    // const user = new User()
+    // user.firstName = "Timber"
+    // user.lastName = "Saw"
+    // user.age = 25
+    // await AppDataSource.manager.save(user)
+    // console.log("Saved a new user with id: " + user.id)
 
-//     // console.log("Loading users from the database...")
-//     // const users = await AppDataSource.manager.find(User)
-//     // console.log("Loaded users: ", users)
+    // console.log("Loading users from the database...")
+    // const users = await AppDataSource.manager.find(User)
+    // console.log("Loaded users: ", users)
 
-//     // console.log("Here you can setup and run express / fastify / any other framework.")
+    // console.log("Here you can setup and run express / fastify / any other framework.")
 
-//     app.listen(port, () => {
-//       console.log(`Server is running on port ${port}`);
-//     });
-//   })
-//   .catch((error) => console.log(error));
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => console.log(error));
