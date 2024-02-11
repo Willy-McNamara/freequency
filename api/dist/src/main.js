f57b7dd2ea053403d60380d32c68e51273eb7489
Willy McNamara <wmcnamara11@gmail.com>
2024-02-11 07:28:15 -0700

    Accessing environment variables from env.local

--- api/dist/src/main.js
+++ api/dist/src/main.js
@@ -2,7 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const dotenv_1 = require("dotenv");
 async function bootstrap() {
+    (0, dotenv_1.config)({ path: '.env.local' });
+    console.log(process.env);
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors();
     await app.listen(3000);
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,7 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
 
 async function bootstrap() {
+  config({ path: '.env.local' });
+  console.log(process.env);
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(3000);
