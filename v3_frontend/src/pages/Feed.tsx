2c8d87770d5a20ac51a0b297139960d8596c3472
Willy McNamara <wmcnamara11@gmail.com>
2025-02-16 11:43:35 -0700

    Mock data added for Feed

--- v3_frontend/src/pages/Feed.tsx
+++ v3_frontend/src/pages/Feed.tsx
@@ -1,14 +1,63 @@
 import React from "react";
 
 const Feed: React.FC = () => {
-  const data = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
+  const posts = [
+    {
+      username: "user1",
+      profilePicture: "path/to/profile1.jpg",
+      date: "2023-10-01",
+      title: "Post 1",
+      description: "Description 1",
+      tasks: ["Task 1", "Task 2"],
+      gasUps: 5,
+      comments: ["Comment 1", "Comment 2"],
+      tags: ["Tag1", "Tag2"],
+      media: {
+        audio: "path/to/audio1.mp3",
+        video: "path/to/video1.mp4",
+        image: "path/to/image1.jpg",
+      },
+    },
+    {
+      username: "user2",
+      profilePicture: "path/to/profile2.jpg",
+      date: "2023-10-02",
+      title: "Post 2",
+      description: "Description 2",
+      tasks: ["Task 3", "Task 4"],
+      gasUps: 3,
+      comments: ["Comment 3", "Comment 4"],
+      tags: ["Tag3", "Tag4"],
+      media: {
+        audio: "path/to/audio2.mp3",
+        video: "path/to/video2.mp4",
+        image: "path/to/image2.jpg",
+      },
+    },
+  ];
 
   return (
     <div>
       <h1>Feed</h1>
       <ul>
-        {data.map((item, index) => (
-          <li key={index}>{item}</li>
+        {posts.map((post, index) => (
+          <li key={index}>
+            <p>Username: {post.username}</p>
+            <p>Profile Picture: {post.profilePicture}</p>
+            <p>Date: {post.date}</p>
+            <p>Title: {post.title}</p>
+            <p>Description: {post.description}</p>
+            <p>Tasks: {post.tasks.join(", ")}</p>
+            <p>Gas Ups: {post.gasUps}</p>
+            <p>Comments: {post.comments.join(", ")}</p>
+            <p>Tags: {post.tags.join(", ")}</p>
+            <p>Media:</p>
+            <ul>
+              <li>Audio: {post.media.audio}</li>
+              <li>Video: {post.media.video}</li>
+              <li>Image: {post.media.image}</li>
+            </ul>
+          </li>
         ))}
       </ul>
     </div>
